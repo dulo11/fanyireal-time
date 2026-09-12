a4934806bb39e7438d7b921eb81ccd943aa88b69
依依 <132633098+dulo11@users.noreply.github.com>
2026-09-12 12:29:45 +0800

    Bump FloatingTranslator to 0.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.zhou.floatingtranslator"
         minSdk = 33
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.4.2"
+        versionCode = 9
+        versionName = "0.4.3"
     }
 
     signingConfigs {
