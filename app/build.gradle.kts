93c485499c12665694bd4021c863c6a90094be0d
依依 <132633098+dulo11@users.noreply.github.com>
2026-09-12 21:32:32 +0800

    Bump v0.6.0 development build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.zhou.floatingtranslator"
         minSdk = 33
         targetSdk = 35
-        versionCode = 19
-        versionName = "0.5.9"
+        versionCode = 20
+        versionName = "0.6.0-dev1"
     }
 
     buildFeatures {
