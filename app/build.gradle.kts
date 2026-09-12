f751f8d4c3b6720489fd346b17bf434c1f893b9e
依依 <132633098+dulo11@users.noreply.github.com>
2026-09-12 18:56:09 +0800

    Bump version to 0.5.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.zhou.floatingtranslator"
         minSdk = 33
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.5.6"
+        versionCode = 17
+        versionName = "0.5.7"
     }
 
     buildFeatures {
