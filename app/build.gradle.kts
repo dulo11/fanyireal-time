e7120ea766eea7972858ae0b5f750296c4c3b263
依依 <132633098+dulo11@users.noreply.github.com>
2026-09-13 10:13:44 +0800

    Reserve versionCode 23 for stable rollback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.zhou.floatingtranslator"
         minSdk = 33
         targetSdk = 35
-        versionCode = 22
+        versionCode = 24
         versionName = "0.6.0-dev3"
     }
 
